110db12a97460c9945f664b1aa0cce067b0b89c8
John Nicholas Suharjono <john.suharjono10@gmail.com>
2022-02-10 12:42:40 +0800

    Update code according to lecture instruction

--- src/main/java/circus/Trainer.java
+++ src/main/java/circus/Trainer.java
@@ -1,3 +1,10 @@
+package circus;
+
+import circus.animal.Animal;
+import circus.animal.Bird;
+import circus.animal.Duck;
+import circus.animal.Parrot;
+
 public class Trainer {
     public static void main(String[] args) {
         Duck d = new Duck();
@@ -9,15 +16,11 @@ public class Trainer {
         Animal a = (Animal) b; // upcasting
         getToSpeak(a);
 
-        Duck d2 = (Duck) a; // downcasting
+        Duck d2 = (Duck) a; // down casting
         getToSpeak(d2);
 
         train(new Duck());
-        // train(new Parrot());
-
-        Animal a2 = new Animal();
-        Bird b2 = new Bird();
-
+        train(new Parrot());
     }
 
     private static void getToSpeak(Animal animal) {
@@ -25,7 +28,11 @@ public class Trainer {
     }
 
     private static void train(Bird bird) {
-        Duck d = (Duck) bird;
-        d.swim();
+        if (bird instanceof Duck) {
+            Duck d = (Duck) bird;
+            d.swim();
+        } else {
+            System.out.println("I am not a duck");
+        }
     }
 }
